f1b5ab7710d634b482b7195b0f78d7e5f9f803ec
unknown <coderboysobuj@gmail.com>
2022-11-17 22:41:25 -0800

    cors options

--- backend/config/allowedOrigins.ts
+++ backend/config/allowedOrigins.ts
@@ -1,5 +1,9 @@
 import * as dotenv from "dotenv";
 dotenv.config();
-const allowedOrigins: Array<string> = [process.env.CLIENT_URL as string];
+const allowedOrigins: Array<string> = [
+  "https://chat-app-frontend.onrender.com",
+  "www.chat-app-frontend.onrender.com",
+  "http://chat-app-frontend.onrender.com",
+];
 
 export default allowedOrigins;
